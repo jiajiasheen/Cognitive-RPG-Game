3ecfc73aa5fc4bd8ccd4a3610a28b5cc37c85c80
jiajiasheen <jiaxing.li@mail.utoronto.ca>
2016-11-07 21:39:54 -0500

    update

--- app/src/main/java/tongzou/cognitivegame/Activity/Fragments/MainStoryFragment.java
+++ app/src/main/java/tongzou/cognitivegame/Activity/Fragments/MainStoryFragment.java
@@ -2,12 +2,15 @@ package tongzou.cognitivegame.Activity.Fragments;
 
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.graphics.drawable.AnimationDrawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import tongzou.cognitivegame.Activity.Activity.MainInterface;
@@ -32,13 +35,34 @@ public class MainStoryFragment extends Fragment{
         hours.setText(hour.toString() + " hours before dawn");
 
         Button tmp_button = (Button) resultView.findViewById(R.id.tmp_btn);
+        Button tmp2_button = (Button) resultView.findViewById(R.id.tmp2_btn);
+        final ImageView runningMan = (ImageView) resultView.findViewById(R.id.somepic);
+        runningMan.post(new Runnable(){
+            @Override
+            public void run() {
+                ((AnimationDrawable) runningMan.getBackground()).start();
+            }
+        });
         tmp_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FragmentManager fragmentManager = getFragmentManager();
                 NBackFragment qi = new NBackFragment();
+                //VisualMemFragment qi = new VisualMemFragment();
                 FragmentTransaction ft = fragmentManager.beginTransaction();
                 ft.replace(R.id.main_interface, qi);
+                //ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
+        tmp2_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                //NBackFragment qi = new NBackFragment();
+                VisualMemFragment vm = new VisualMemFragment();
+                FragmentTransaction ft = fragmentManager.beginTransaction();
+                ft.replace(R.id.main_interface, vm);
                 //ft.addToBackStack(null);
                 ft.commit();
             }
